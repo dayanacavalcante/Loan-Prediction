1d1eba2c99e4f2e07139931329f32d09f18a05d8
dayanacavalcante <d.liria@gmail.com>
2021-01-08 13:07:14 +0000

    Change to README.md and code

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ In this form the client must inform the following data: Sex, Marital Status, Edu
 
 It is a classification problem as it will be foreseen whether the loan will be accepted or not.
 
-Follow the link for the data:
+The data was taken from Kaggle. Follow the link:
 https://www.kaggle.com/burak3ergun/loan-data-set?select=loan_data_set.csv
 
 ### _Exploratory Data Analysis_
@@ -18,6 +18,13 @@ Having a visual summary of the information makes it easier to identify patterns 
 |:-:|:-:|:-:|
 | ![](/Graphics/Credit_History.png) | ![](./Graphics/Property_Area.png) | ![](./Graphics/Loan_Amount_Term.png)|
 
+Through the graphics we can verify:
+- The predominance of loan applications by men, as well as the largest number of loans for this gender;
+- Virtually twice as many loan applications by married people and twice as many loan approvals for this category;
+- The prevalence of requests by non-self-employed people and the approval of loans for this category;
+- Predominance of loan applications and approvals by people with a good credit history;
+- Slightly higher loan approval and requests for the semi-urban population than for the urban population;
+- Prevalence of loan approval for credits and high period of time;
 ### _Transforming categorical variables into numeric ones_
 To work with machine learning we need to transform categorical variables into numeric ones. For this I used the pandas get_dummies method.
 
@@ -25,7 +32,18 @@ To work with machine learning we need to transform categorical variables into nu
 For this classification problem I used the following algorithms: Logistic Regression, Decision Tree and Random Forest.
 
 ### _Performance Metrics_
-Through the F1_Score metric, it can be seen that Logistic Regression performed better than the three applied algorithms.
+I chose to use the F1_Score metric which is a harmonic average of the precision and recall metrics. I did not choose to use accuracy because for unbalanced data sets it is not advisable. It can be seen that Logistic Regression performed slightly better than Random Forest Classifier and the worst performance of the three algorithms was with Decision Tree.
+
+Finally, I applied the Confusion Matrix, which is a great tool to assess behavior and understand the effectiveness of a binary or categorical classifier.
+
+The Confusion Matrix is a two-dimensional matrix that compares the predicted category labels with the actual labels.
+
+| ![](/Graphics/ConfusionMatrix_LogisticRegression.png) | ![](./Graphics/ConfusionMatrix_DecisionTree.png) | ![](./Graphics/ConfusionMatrix_RandomForestClassifier.png)|
+|:-:|:-:|:-:|
+
+Through the Confusion Matrix it can be seen that the performance of the Logistic Regression and Random Forest Classifier was practically the same and a worse performance in the classification through the Decision Tree.
+
+
 
 
 
--- Src/upload_data.py
+++ Src/upload_data.py
@@ -1,26 +1,24 @@
-
 # Imports
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
 
 # Loading Data
 data = pd.read_csv('C:\\Users\\RenanSardinha\\Documents\\Data Science\\Loan Prediction\\Data\\loan_data_set.csv')
-print("############################## DATA HEAD ##############################")
 print(data.head())
-print("############################## END DATA HEAD ##############################")
 
 print(data.shape)
 
 # Data Preparation
-print("############################## DATA ISNULL SUM ##############################")
 print(data.isnull().sum())
-print("############################## END DATA ISNULL SUM ##############################")
 
 data['Gender'].fillna(data['Gender'].mode()[0],inplace=True)
 data['Married'].fillna(data['Married'].mode()[0],inplace=True)
@@ -34,9 +32,7 @@ data['LoanAmount'].fillna(128,inplace=True)
 print(data['Loan_Amount_Term'].value_counts())
 data['Loan_Amount_Term'].fillna(360,inplace=True)
 
-print("############################## DATA ISNULL SUM ##############################")
 print(data.isnull().sum())
-print("############################## END DATA ISNULL SUM ##############################")
 
 print(data.apply(lambda x: len(x.unique())))
 
@@ -47,38 +43,31 @@ sns.countplot(x='Gender', hue='Loan_Status', data=data, palette='Set3')
 sns.countplot(x='Married', hue='Loan_Status', data=data, palette='Set3')
 #%%
 sns.countplot(x='Self_Employed', hue='Loan_Status', data=data, palette='Set3')
-# %%
+#%%
 sns.countplot(x='Credit_History', hue='Loan_Status', data=data, palette='Set3')
 #%%
 sns.countplot(x='Property_Area', hue='Loan_Status', data=data, palette='Set3')
 #%%
 sns.countplot(x='Loan_Amount_Term', hue='Loan_Status', data=data, palette='Set3')
-#%%
 
 # Separate the variables between predictors and target
+#%%
 x = data.drop(['Loan_ID', 'Loan_Status'], axis=1)
 y = data['Loan_Status']
-
-print("############################## X ##############################")
+#%%
 print(x.head())
-print("############################## END X ##############################")
-
-print("############################## Y ##############################")
 print(y.head())
-print("############################## END Y ##############################")
 
 # Transforming categorical variables into numeric ones
+#%%
 x = pd.get_dummies(x)
-print("############################## X DUMMIES ##############################")
 print(x.head())
-print("############################## END X DUMMIES ##############################")
-y = data['Loan_Status'].replace('Y', 1).replace('N', 0)
 
-print("############################## Y REPLACE ##############################")
+y = data['Loan_Status'].replace('Y', 1).replace('N', 0)
 print(y.head())
-print("############################## Y REPLACE ##############################")
 
 # Create the training and test dataset
+#%%
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=.3,random_state=2)
 
 print(x_train.shape)
@@ -86,40 +75,87 @@ print(y_train.shape)
 
 # Model Training
 ## Logistic Regression
-model=LogisticRegression(max_iter=100)
+#%%
+model=LogisticRegression()
 model.fit(x_train,y_train)
 
 ## Decision Tree
+#%%
 tree=DecisionTreeClassifier()
 tree.fit(x_train,y_train)
 
 ## Random Forest Classifier
+#%%
 forest=RandomForestClassifier()
 forest.fit(x_train,y_train)
 
 # Performance Metrics
 ## Logistic Regression
+#%%
 ypred=model.predict(x_test)
 print(ypred)
 
 evaluation=f1_score(y_test,ypred)
 print(evaluation)
 
-print(y_test[0:2])
-print(x_test[0:2])
-ypred=model.predict(x_test[0:2])
-print(ypred)
+### Confusion Matrix
+#%%
+true_labels=y_test
+pred_labels=ypred
+
+confusion_matrix=confusion_matrix(true_labels,pred_labels)
+sns.heatmap(confusion_matrix,annot=True,fmt='g',cmap=plt.cm.Greens)
+plt.title("Confusion Matrix Logistic Regression")
+ticks=np.arange(2)
+plt.xticks=(ticks,ticks)
+plt.yticks=(ticks,ticks)
+plt.xlabel("Predict")
+plt.ylabel("Real")
+plt.show()
 
 ## Decision Tree
+#%%
 ypred_tree=tree.predict(x_test)
 print(ypred_tree)
 
 evaluation_tree=f1_score(y_test,ypred_tree)
 print(evaluation_tree)
 
+### Confusion Matrix
+#%%
+true_labels=y_test
+pred_labels=ypred_tree
+
+confusion_matrix=confusion_matrix(true_labels,pred_labels)
+sns.heatmap(confusion_matrix,annot=True,fmt='g',cmap=plt.cm.Greens)
+plt.title("Confusion Matrix Decision Tree")
+ticks=np.arange(2)
+plt.xticks=(ticks,ticks)
+plt.yticks=(ticks,ticks)
+plt.xlabel("Predict")
+plt.ylabel("Real")
+plt.show()
+
 ## Random Forest Classifier
+#%%
 ypred_forest=forest.predict(x_test)
 print(ypred_forest)
 
 evaluation_forest=f1_score(y_test,ypred_forest)
 print(evaluation_forest)
+
+### Confusion Matrix
+#%%
+true_labels=y_test
+pred_labels=ypred_forest
+
+confusion_matrix=confusion_matrix(true_labels,pred_labels)
+sns.heatmap(confusion_matrix,annot=True,fmt='g',cmap=plt.cm.Greens)
+plt.title("Confusion Matrix Random Forest Classifier")
+ticks=np.arange(2)
+plt.xticks=(ticks,ticks)
+plt.yticks=(ticks,ticks)
+plt.xlabel("Predict")
+plt.ylabel("Real")
+plt.show()
+# %%
