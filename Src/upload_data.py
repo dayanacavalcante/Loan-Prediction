701543f1c5d2ada80272504eff5daf235231791e
dayanacavalcante <d.liria@gmail.com>
2021-02-15 19:35:28 +0000

    Modified code

--- Src/upload_data.py
+++ Src/upload_data.py
@@ -1,5 +1,5 @@
 # Imports
-#%%
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -37,125 +37,108 @@ print(data.isnull().sum())
 print(data.apply(lambda x: len(x.unique())))
 
 # Exploratory Data Analysis
-#%%
+
 sns.countplot(x='Gender', hue='Loan_Status', data=data, palette='Set3')
-#%%
+
 sns.countplot(x='Married', hue='Loan_Status', data=data, palette='Set3')
-#%%
+
 sns.countplot(x='Self_Employed', hue='Loan_Status', data=data, palette='Set3')
-#%%
+
 sns.countplot(x='Credit_History', hue='Loan_Status', data=data, palette='Set3')
-#%%
+
 sns.countplot(x='Property_Area', hue='Loan_Status', data=data, palette='Set3')
-#%%
+
 sns.countplot(x='Loan_Amount_Term', hue='Loan_Status', data=data, palette='Set3')
 
 # Separate the variables between predictors and target
-#%%
-x = data.drop(['Loan_ID', 'Loan_Status'], axis=1)
+
+X = data.drop(['Loan_ID', 'Loan_Status'], axis=1)
 y = data['Loan_Status']
-#%%
-print(x.head())
+
+print(X.head())
 print(y.head())
 
 # Transforming categorical variables into numeric ones
-#%%
-x = pd.get_dummies(x)
-print(x.head())
+
+X = pd.get_dummies(X)
+print(X.head())
 
 y = data['Loan_Status'].replace('Y', 1).replace('N', 0)
 print(y.head())
 
 # Create the training and test dataset
-#%%
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=.3,random_state=2)
 
-print(x_train.shape)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.3,random_state=2)
+
+print(X_train.shape)
 print(y_train.shape)
 
 # Model Training
 ## Logistic Regression
-#%%
-model=LogisticRegression()
-model.fit(x_train,y_train)
+
+model = LogisticRegression()
+model.fit(X_train,y_train)
 
 ## Decision Tree
-#%%
-tree=DecisionTreeClassifier()
-tree.fit(x_train,y_train)
+
+tree = DecisionTreeClassifier()
+tree.fit(X_train,y_train)
 
 ## Random Forest Classifier
-#%%
-forest=RandomForestClassifier()
-forest.fit(x_train,y_train)
+
+forest = RandomForestClassifier()
+forest.fit(X_train,y_train)
 
 # Performance Metrics
 ## Logistic Regression
-#%%
-ypred=model.predict(x_test)
+
+ypred = model.predict(X_test)
 print(ypred)
 
 evaluation=f1_score(y_test,ypred)
-print(evaluation)
+print('f1_score of Logistic Regression Classifier on test set: {:.2}'.format(evaluation))
 
 ### Confusion Matrix
-#%%
-true_labels=y_test
-pred_labels=ypred
 
-confusion_matrix=confusion_matrix(true_labels,pred_labels)
-sns.heatmap(confusion_matrix,annot=True,fmt='g',cmap=plt.cm.Greens)
+cmlr=confusion_matrix(y_test,ypred)
+sns.heatmap(cmlr,annot=True,fmt='g',cmap="Greens")
 plt.title("Confusion Matrix Logistic Regression")
-ticks=np.arange(2)
-plt.xticks=(ticks,ticks)
-plt.yticks=(ticks,ticks)
 plt.xlabel("Predict")
 plt.ylabel("Real")
 plt.show()
 
 ## Decision Tree
-#%%
-ypred_tree=tree.predict(x_test)
+
+ypred_tree = tree.predict(X_test)
 print(ypred_tree)
 
 evaluation_tree=f1_score(y_test,ypred_tree)
 print(evaluation_tree)
+print('f1_score of Decision Tree Classifier on test set: {:.2}'.format(evaluation_tree))
 
 ### Confusion Matrix
-#%%
-true_labels=y_test
-pred_labels=ypred_tree
 
-confusion_matrix=confusion_matrix(true_labels,pred_labels)
-sns.heatmap(confusion_matrix,annot=True,fmt='g',cmap=plt.cm.Greens)
+cmdt=confusion_matrix(y_test,ypred_tree)
+sns.heatmap(cmdt,annot=True,fmt='g',cmap="Greens")
 plt.title("Confusion Matrix Decision Tree")
-ticks=np.arange(2)
-plt.xticks=(ticks,ticks)
-plt.yticks=(ticks,ticks)
 plt.xlabel("Predict")
 plt.ylabel("Real")
 plt.show()
 
 ## Random Forest Classifier
-#%%
-ypred_forest=forest.predict(x_test)
+
+ypred_forest=forest.predict(X_test)
 print(ypred_forest)
 
 evaluation_forest=f1_score(y_test,ypred_forest)
 print(evaluation_forest)
+print('f1_score of Random Forest Classifier on test set: {:.2}'.format(evaluation_forest))
 
 ### Confusion Matrix
-#%%
-true_labels=y_test
-pred_labels=ypred_forest
 
-confusion_matrix=confusion_matrix(true_labels,pred_labels)
-sns.heatmap(confusion_matrix,annot=True,fmt='g',cmap=plt.cm.Greens)
+cmrf=confusion_matrix(y_test,ypred_forest)
+sns.heatmap(cmrf,annot=True,fmt='g',cmap="Greens")
 plt.title("Confusion Matrix Random Forest Classifier")
-ticks=np.arange(2)
-plt.xticks=(ticks,ticks)
-plt.yticks=(ticks,ticks)
 plt.xlabel("Predict")
 plt.ylabel("Real")
 plt.show()
-# %%
